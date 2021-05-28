a126618f6be644841977267e48b0c8455e004374
Vinay <Vinay@VINAYAK>
2021-05-28 14:48:24 +0530

    update

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var menulist = document.getElementById("menulist");
+
+menulist.style.maxHeight = "0px";
+
+function togglemenu(){
+    if(menulist.style.maxHeight == "0px")
+    {
+        menulist.style.maxHeight = "130px"
+    }
+    else
+    {
+        menulist.style.maxHeight = "0px"
+    }
+}
+
+function click1(){
+   location.href="First.html";
+}
